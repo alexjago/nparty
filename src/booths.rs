9980020b3b4433adf5a675ed6d962d130e0a2303
Alex Jago <4475543+alexjago@users.noreply.github.com>
2022-07-12 10:59:42 +1000

    Performance: string interning and HashMaps

--- src/booths.rs
+++ src/booths.rs
@@ -1,25 +1,34 @@
 //! The n-party-preferred distribution phase.
 /// We want to reduce each unique preference sequence to some ordering
 ///    of each of the parties. For example, for four parties there are 65 orderings:
-///   (0!) + (4 * 1!) + (6 * 2!) + (4 * 3!) + (4!)
+///   `(0!) + (4 * 1!) + (6 * 2!) + (4 * 3!) + (4!)`
+///
+/// In fact, there are even more orderings (voters might interleave candidates)
+/// but we will consider the most-preferred candidate from each party as
+/// representing it (e.g. a vote `A1 > B1 > B2 > B3 > A2 > A3` as `A > B`).
 use color_eyre::eyre::{bail, eyre, Context, ContextCompat, Result};
 use color_eyre::Section;
 use factorial::Factorial;
 use indexmap::IndexMap;
 use itertools::Itertools;
-use std::collections::{BTreeMap, HashMap};
+use std::collections::HashMap;
 use std::fs::create_dir_all;
 use std::path::Path;
+use string_interner::{backend::StringBackend, symbol::SymbolU16, StringInterner};
 use tracing::{info, trace};
 
 use super::term;
 use super::utils::{fix_prefs_headers, open_csvz_from_path, StateAb};
 
+/// The output file will start with these five columns:
+/// Booth ID, division name, booth name, latitude and longitude.
 const NPP_FIELD_NAMES: [&str; 5] = ["ID", "Division", "Booth", "Latitude", "Longitude"];
 // for the output
 // const BOOTH_FIELD_NAMES :[&str; 15] = ["State", "DivisionID", "DivisionNm", "PollingPlaceID", "PollingPlaceTypeID", "PollingPlaceNm",
 //                               "PremisesNm", "PremisesAddress1", "PremisesAddress2", "PremisesAddress3", "PremisesSuburb",
 //                               "PremisesStateAb", "PremisesPostCode", "Latitude", "Longitude"];
+
+/// Preferences files in the 2019+ format begin with these six columns.
 const PREFS_FIELD_NAMES: [&str; 6] = [
     "State",
     "Division",
@@ -50,7 +59,7 @@ fn ttyjump() -> &'static str {
     }
 }
 
-/// Returns the sum of (1!...input!)
+/// Returns the sum of (1!, ..., input!)
 pub fn factsum(input: usize) -> usize {
     let mut output: usize = 0;
     for i in 1..=input {
@@ -59,9 +68,11 @@ pub fn factsum(input: usize) -> usize {
     output
 }
 
+/// Construct all the orderings of the specified groups.
+///
+/// (i.e. the sequence of permutations of the groups,
+/// from length 0 to length N)
 pub fn group_combos(groups: &[&str]) -> Vec<String> {
-    //! Generates a Vec<String> of all the orderings
-
     let mut combinations = Vec::with_capacity(factsum(groups.len()));
     combinations.push(String::from("None"));
 
@@ -75,9 +86,15 @@ pub fn group_combos(groups: &[&str]) -> Vec<String> {
     combinations
 }
 
-/// This function exists to avoid a *lot* of string alloc
-pub fn make_combo_tree(groups_count: usize) -> BTreeMap<Vec<usize>, usize> {
-    let mut output: BTreeMap<Vec<usize>, usize> = BTreeMap::new();
+/// A "combo tree" is a map of the indexes of party orderings.  
+/// Suppose we had N parties labelled as 0 through N-1. This allows us to
+/// take an *ordering* of those parties, say `[0, 1]` and get the corresponding
+/// index in the output of [`group_combos`]. In turn, that index can stand for
+/// the ordering (which is itself a reduction of the ballot).
+///
+/// tl;dr this function exists to avoid a *lot* of string allocations.
+pub fn make_combo_tree(groups_count: usize) -> HashMap<Vec<usize>, usize> {
+    let mut output: HashMap<Vec<usize>, usize> = HashMap::new();
     output.insert(vec![], 0_usize);
     let mut c: usize = 1;
     for r in 1..=groups_count {
@@ -94,7 +111,7 @@ pub fn make_combo_tree(groups_count: usize) -> BTreeMap<Vec<usize>, usize> {
 /// This represents a row in the `polling_places` file
 #[derive(Debug, Deserialize)]
 #[allow(non_snake_case)] // look, this isn't aesthetic but it matches the file
-#[allow(dead_code)]
+#[allow(dead_code)] // most of these columns aren't actually used
 pub struct BoothRecord {
     State: StateAb,
     DivisionID: usize,
@@ -114,12 +131,16 @@ pub struct BoothRecord {
 }
 
 /// A (Division, Booth) combination
-pub type DivBooth = (String, String);
-/// A map from the party name to a list of (pseudo)candidates
+type DivBooth = (SymbolU16, SymbolU16);
+
+/// A map from the party name to a list of (pseudo)candidates of that party.
 pub type Parties = IndexMap<String, Vec<String>>;
 
-// TODO: make this take Read objects instead of paths.
-//       otherwise it'll never work in WASM.
+/// Perform the distribution over a specified set of parties.
+///
+/// * `formal_prefs_path`: the input preferences (one row per ballot)
+/// * `polling_places_path`: the input info on polling places
+/// * `npp_booths_path`: where to write the output.
 pub fn booth_npps(
     parties: &Parties,
     state: StateAb,
@@ -127,6 +148,8 @@ pub fn booth_npps(
     polling_places_path: &Path,
     npp_booths_path: &Path,
 ) -> Result<()> {
+    // TODO: make this take Read objects instead of paths.
+    //       otherwise it'll never work in WASM.
     info!("\tDistributing Preferences");
     let mut partykeys = Vec::with_capacity(parties.len());
     for i in parties.keys() {
@@ -141,31 +164,22 @@ pub fn booth_npps(
     }
     let party_indices = party_indices;
 
-    let combo_tree = make_combo_tree(partykeys.len());
-
     let combinations = group_combos(&partykeys);
     trace!("Combinations:\n{:#?}", combinations);
-
-    // this should get obsoleted by the combo_tree method
-    let mut combo_order: HashMap<String, usize> = HashMap::new();
-    combo_order.insert("None".to_string(), 0);
-    let mut c: usize = 1;
-    for i in &combinations {
-        combo_order.insert(i.clone(), c);
-        c += 1;
-    }
+    let combo_tree = make_combo_tree(partykeys.len());
 
     trace!(
-        "Combo tree:\n{}",
+        "Combo tree:\nOrder\tIndex\tPreference\n{}",
         combo_tree
             .iter()
-            .map(|(k, v)| format!("{:?}:\t{:?}\t{}", k, v, combinations[*v]))
+            .map(|(k, v)| format!("{:?}\t{:5}\t{}", k, v, combinations[*v]))
             .join("\n")
     );
 
     // this is now just for actual booth data
     // For some gods-forsaken reason, the PollingPlaceID is not the Vote Collection Point ID
-    // The only consistent identifier is {Division}_{Booth}
+    // The only consistent identifier is ({Division}, {Booth})
+    let mut interner = StringInterner::<StringBackend<SymbolU16>>::new();
     let mut booths: HashMap<DivBooth, BoothRecord> = HashMap::new();
 
     // but here we use Serde
@@ -198,8 +212,9 @@ pub fn booth_npps(
         if record.State != state {
             continue;
         }
-
-        let dvb = (record.DivisionNm.clone(), record.PollingPlaceNm.clone());
+        let division_nm = interner.get_or_intern(record.DivisionNm.clone());
+        let booth_nm = interner.get_or_intern(record.PollingPlaceNm.clone());
+        let dvb = (division_nm, booth_nm);
         booths.insert(dvb, record);
     }
 
@@ -247,16 +262,16 @@ pub fn booth_npps(
 
     // Create candidate number index
 
-    let mut cand_nums: BTreeMap<&str, usize> = BTreeMap::new();
+    let mut cand_nums: HashMap<&str, usize> = HashMap::new();
     for (i, pref) in prefs_headers_fixed.iter().skip(above_start).enumerate() {
         cand_nums.insert(pref, 1 + i);
     }
     let cand_nums = cand_nums; // make immutable now
 
     // set up some lookups...
-    let mut groups: BTreeMap<usize, Vec<usize>> = BTreeMap::new();
-    let mut groups_above: BTreeMap<usize, Vec<usize>> = BTreeMap::new();
-    let mut groups_below: BTreeMap<usize, Vec<usize>> = BTreeMap::new();
+    let mut groups: HashMap<usize, Vec<usize>> = HashMap::new();
+    let mut groups_above: HashMap<usize, Vec<usize>> = HashMap::new();
+    let mut groups_below: HashMap<usize, Vec<usize>> = HashMap::new();
 
     for (party, cand_list) in parties.iter() {
         let mut party_cand_nums = Vec::new();
@@ -288,7 +303,7 @@ pub fn booth_npps(
         a.sort_by(|&(_, a), &(_, b)| a.cmp(&b));
         a.iter().map(|(s, u)| format!("{:4}\t{}", u, s)).join("\n")
     });
-    trace!("\nFull Groups: {:?}", groups);
+    trace!("Full Groups: {:?}", groups);
     trace!("ATL Groups: {:?}", groups_above);
     trace!("BTL Groups: {:?}", groups_below);
 
@@ -299,42 +314,34 @@ pub fn booth_npps(
     // this is where we're going to store all the things
     let mut booth_counts: HashMap<DivBooth, Vec<usize>> = HashMap::new();
 
-    let mut division_specials: BTreeMap<DivBooth, Vec<usize>> = BTreeMap::new();
-
     let cands_count = (&prefs_headers_fixed).len() - above_start;
-
-    // we need to figure out how we're going to deserialize each record
-    // or not - we need custom logic for most of it anyway
 
     let prefs_rdr_iter = prefs_rdr.records();
     let mut progress: usize = 0;
 
-    // performance note: we tried amortizing the allocation and it was slower ?!?!?!?!?!
-    // and then we tried again and it was faster.
-    // let mut record = csv::StringRecord::new();
-    // while prefs_rdr.read_record(&mut record)? {
-
+    // We won't do a "magic" deserialisation as only a few initial columns
+    // are the same between different ballot files
     for row in prefs_rdr_iter {
         let record = row?;
 
-        let divnm = &record[1];
-        let boothnm = &record[2];
+        let divnm = interner.get_or_intern(&record[1]);
+        let boothnm = interner.get_or_intern(&record[2]);
 
-        if divnm.starts_with("---") {
-            // TODO if this is a NOT, it is inverted for testing
+        if (&record[1]).starts_with("---") {
+            // ^^ This conditional might be inverted for testing; 2019+ files do NOT contain a `---` line.
             return Result::Err(eyre!("Preferences file is in the 2016 format."))
                 .suggestion("Upgrade the file to the 2019+ format with:\n\tnparty upgrade prefs");
         }
 
-        // Now we analyse nPP. We categorise the preference sequence by its highest value for each group of candidates
+        // Now we analyse nPP. We categorise the preference sequence by its highest value for each group of candidates.
 
-        // first we must determine if it's ATL or BTL.
+        // First we must determine if it's ATL or BTL.
+
         // Per section 268A of the Commonwealth Electoral Act, a vote is BTL-formal if it has at least [1] through [6] marked BTL
         // and BTL-formality takes priority over ATL-formality.
         // NOTE 2020-05-14: I am quite confident this ATL-vs-BTL code is correct. It produces the correct number of BTLs...
-        // To be fair, non-BTLs don't have trailing commas to confuse the issue with...
         let mut is_btl: bool = false; // we must test whether it is, but...
-        let bsa = above_start + below_start; // btl_start absolute
+        let bsa = above_start + below_start; // BTL, start, absolute
         if record.len() > bsa {
             // ^^^ this is the actual biggest speedup for default 2019 files.
             // If there aren't any fields for BTLs, there aren't any at all...
@@ -345,10 +352,6 @@ pub fn booth_npps(
                                                      // ... and someone plays *extreme* silly buggers
 
             for v in record.iter().skip(bsa) {
-                // .skip() is a glorious thing, how did I not know it before
-                // NB: this breaks if there's whitespace in the file.
-                // We need to trim().
-                // Given the whole lack-of-trailing-comma-sitch it won't even hurt too much
                 match v.trim() {
                     "1" => btl_counts[0] += 1,
                     "2" => btl_counts[1] += 1,
@@ -362,47 +365,42 @@ pub fn booth_npps(
             is_btl = btl_counts.iter().all(|c| *c == 1);
         }
 
+        // Select the appropriate candidates
         let groups_which = if is_btl { &groups_below } else { &groups_above };
         if is_btl {
             btl_count += 1;
         }
 
-        // This next bit is a little complicated. What we do is pre-generate a list of parties,
-        // as well as a tree of combinations of parties, and we index by that. This avoids messing
-        // about with strings as much as we can, especially in the inner loop.
-
+        // Having determined the ballot's ATL/BTL status we determine the "best" preference
+        // (if any) for each party we care about. We take only the "best" if candidates
+        // are interleaved. Thus `bests` is a list of `(preference, party number)` pairs.
+        // (Each party has an implicit number, like we mentioned in [make_combo_tree]).
         let mut bests: Vec<(usize, usize)> = Vec::with_capacity(partykeys.len());
 
-        for (p, cns) in groups_which.iter() {
-            let mut curbest: usize = cands_count; // this heavily reduced HashMapping
-            for i in cns {
-                let bal = match record.get(i + above_start - 1) {
-                    // always check: is this the right offset?
-                    Some(x) => match x.trim().parse::<usize>() {
-                        Ok(n) => n,
-                        Err(_) => continue,
-                    },
-                    None => continue,
-                };
-                if bal < curbest {
-                    curbest = bal;
+        for (party_num, candidate_nums) in groups_which.iter() {
+            // Manually iterate over the candidates and get the best preferenced.
+            // There's unlikely to be more than about 13 per party, so O(n) is OK.
+            let mut cur_best: usize = cands_count;
+            for i in candidate_nums {
+                if let Some(x) = record.get(i + above_start - 1) {
+                    // ^^ always check: is this the right offset?
+                    if let Ok(bal) = x.trim().parse::<usize>() {
+                        if bal < cur_best {
+                            cur_best = bal;
+                        }
+                    }
                 }
             }
-            if curbest < cands_count {
-                bests.push((curbest, *p));
+            if cur_best < cands_count {
+                bests.push((cur_best, *party_num));
             }
         }
-
+        // sort to order them
         bests.sort_unstable();
         let order: Vec<usize> = bests.iter().map(|x| x.1).collect();
         let pref_idx = *combo_tree.get(&order).context("no pref index?")?;
 
-        // Using strings here is surely one of the slower parts of the operation
-        // Actually this datastructure in general is one of the slower things.
-        // Seems very difficult to avoid keying by DivBooth though.
-        // But datastructure stuff is responsible for almost 20% of booths.rs runtime
-
-        let divbooth: DivBooth = (divnm.to_string(), boothnm.to_string());
+        let divbooth: DivBooth = (divnm, boothnm);
 
         let booth = booth_counts
             .entry(divbooth)
@@ -427,15 +425,26 @@ pub fn booth_npps(
         progress,
         btl_count
     );
+
+    trace!(
+        "We interned {} strings out of a possible {}.",
+        interner.len(),
+        u16::MAX
+    );
+
+    // Initially, the special votes are split up into e.g. POSTAL_1 through POSTAL_8
+    // This isn't useful for us, so we'll aggregate them.
     info!("\t\tAggregating Absents, Postals, Prepolls & Provisionals");
+
+    let mut division_specials: HashMap<DivBooth, Vec<usize>> = HashMap::new();
 
     let mut to_remove = Vec::new();
 
     // What we're doing here is aggregating all special votes.
     for (bk, bv) in &booth_counts {
         for w in &NON_BOOTH_CONVERT {
-            if bk.1.contains(w) {
-                let divbooth: DivBooth = (bk.0.clone(), non_booth_convert(w).to_string());
+            if interner.resolve(bk.1).unwrap().contains(w) {
+                let divbooth: DivBooth = (bk.0, interner.get_or_intern(non_booth_convert(w)));
                 let db = division_specials
                     .entry(divbooth)
                     .or_insert_with(|| vec![0_usize; bv.len()]);
@@ -443,7 +452,7 @@ pub fn booth_npps(
                     db[j] += bv[j];
                 }
                 // ^^ Still not sure I like this version. We didn't need to do the addition on new entries before.
-                to_remove.push(bk.clone());
+                to_remove.push(*bk);
                 break;
             }
         }
@@ -504,8 +513,8 @@ pub fn booth_npps(
     for (bk, bv) in &division_specials {
         let mut bdeets: Vec<String> = vec![
             "".to_string(),
-            bk.0.clone(),
-            bk.1.clone(),
+            interner.resolve(bk.0).unwrap().to_string(),
+            interner.resolve(bk.1).unwrap().to_string(),
             "".to_string(),
             "".to_string(),
         ];
@@ -523,3 +532,39 @@ pub fn booth_npps(
 
     Ok(())
 }
+
+/*
+    *** Performance and string interning ***
+
+    Because PollingPlaceID and VoteCollectionPointID aren't the same thing,
+    we need to use (Division, Booth) as our key. This poses a bit of an issue:
+    the StringRecord iterator gives us an &str to the fields, but
+    (a) the lifetime is only good for that iteration
+    (b) the types don't match for Map operations
+
+    So originally, DivBooths were `(String, String)` and we cloned extensively.
+    However, using string interning offers a potential benefit: we can swap out
+    our Strings for Symbols (which are just uints in disguise).
+    This also has HashMap benefits, so we'll have to test non-interned HashMaps
+
+    *** Results ***
+
+    Relative result notes from `cargo-flamegraph`, main datastructures are HashMaps
+
+    * with interning we seem to be running at about 4% of runtime on interning?
+    * without it we spend about 4% of time on alloc::borrow::ToOwned which doesn't show up with interning
+    * we also spend WAY more time in BTree::entry, 17% vs 4%
+
+    Absolute result notes from timing it (obviously machine-specific, but indicative):
+    `for i in {1..10}; do time ./target/release/nparty -qq run -p distribute -s QLD_4PP configurations/2022.toml; done`
+    (Default hashers, U16 symbols - QLD-2022 for example has < 2000 strings to intern.)
+
+    * Interning and main structure BTreeMap: 3.11 mean, 3.17 max, 3.04 min
+    * Interning and main structure HashMap: 3.09 mean, 3.15 max, 3.04 min
+    * String/cloning and main structure BTreeMap: 3.74 mean, 3.83 max, 3.71 min
+    * String/cloning and main structure HashMap: 3.38 mean, 3.46 max, 3.32 min
+
+    So interning is faster, but really not by much. A larger saving was just in switching to HashMap.
+    Still, we got a 17% speedup for this phase all told and that's pretty good.
+
+*/
